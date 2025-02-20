7e9a8fb3f063d3438de12084b0b1bdd518ed4c5b
EliwtFdez <shadowkadizz@gmail.com>
2025-02-19 22:56:19 -0600

    definir bien la cantidad de decimales

--- Entities/Deuda.cs
+++ Entities/Deuda.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClusterWeb.Entities
 {
@@ -7,14 +8,19 @@ namespace ClusterWeb.Entities
         public int DeudaId { get; set; }
         public int ResidenteId { get; set; }
         public int CasaId { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Monto { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
         public decimal SaldoPendiente { get; set; }
+
         public DateTime FechaVencimiento { get; set; }
         public string Estado { get; set; } = "pendiente";
         public string Descripcion { get; set; }
         public DateTime FechaRegistro { get; set; } = DateTime.Now;
 
         public Residente Residente { get; set; }
-        public Casa? Casa { get; set; }
+        public Casa Casa { get; set; }
     }
 }
--- Entities/Pago.cs
+++ Entities/Pago.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClusterWeb.Entities
 {
@@ -7,11 +8,14 @@ namespace ClusterWeb.Entities
         public int PagoId { get; set; }
         public int DeudaId { get; set; }
         public int ResidenteId { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
         public decimal MontoPagado { get; set; }
+
         public DateTime FechaPago { get; set; } = DateTime.Now;
         public string MetodoPago { get; set; }
 
         public Deuda Deuda { get; set; }
-        public Residente? Residente { get; set; }
+        public Residente Residente { get; set; }
     }
 }
